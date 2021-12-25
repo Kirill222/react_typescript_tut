5b4f854476fdbda65b108053165838fdf0863b09
Zzz <zkmd2012@inbox.ru>
2021-12-25 15:00:47 +0100

    Typing handle change event for input component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import './App.css';
 import { Button } from './components/Button';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Status } from './components/Status';
 
 function App() { 
   
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('clicked', event)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>, id: number) => {
+    console.log('clicked', event, id)
   }
 
   return (
     <div className="App">
       <Button handleClick={handleClick} />
+      <Input value='' handleChange={event => console.log(event)} />
     </div>
   );
 }
